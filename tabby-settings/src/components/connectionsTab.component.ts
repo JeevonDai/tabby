6b6f77defe0915dfcd015ce2914a60d56e20d30e
JeevonDai <202373657@qq.com>
2026-07-15 22:10:17 +0800

    优化连接标签页横向滚动布局

--- tabby-settings/src/components/connectionsTab.component.ts
+++ tabby-settings/src/components/connectionsTab.component.ts
@@ -5,7 +5,8 @@ import { BaseTabComponent, TranslateService } from 'tabby-core'
 /** @hidden */
 @Component({
     selector: 'connections-tab',
-    template: '<profiles-settings-tab initialSubTab="connections"></profiles-settings-tab>',
+    template: '<div class="connections-tab-body" tabindex="0" (wheel)="onWheel($event)"><profiles-settings-tab initialSubTab="connections"></profiles-settings-tab></div>',
+    styleUrls: ['./connectionsTab.component.scss'],
 })
 export class ConnectionsTabComponent extends BaseTabComponent {
     constructor (
@@ -15,5 +16,13 @@ export class ConnectionsTabComponent extends BaseTabComponent {
         super(injector)
         this.setTitle(translate.instant(_('Connections')))
         this.icon = 'fas fa-network-wired'
+    }
+
+    onWheel (event: WheelEvent): void {
+        const target = event.currentTarget as HTMLElement
+        if (Math.abs(event.deltaY) > Math.abs(event.deltaX)) {
+            target.scrollLeft += event.deltaY
+            event.preventDefault()
+        }
     }
 }
